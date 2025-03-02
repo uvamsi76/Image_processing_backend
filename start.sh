244f6f45dff8d7465dcc5b500fcc9894f4fd7513
Vamsi Uppuluri <uvamsi76@gmail.com>
2025-03-02 11:32:51 +0530

    added a small commit/change

--- start.sh
+++ start.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
 
+VENV_DIR="venv"
+
+if [ -d "$VENV_DIR" ]; then
+    echo "Virtual environment already exists."
+else
+    echo "Creating virtual environment..."
+    python3 -m venv "$VENV_DIR"
+    echo "Virtual environment created successfully."
+fi
+
+source $VENV_DIR/bin/activate
+
 pip install -r requirements.txt
 
 echo "Uvmowa@2024" | sudo -S apt install redis-server -y
--- stop.sh
+++ stop.sh
@@ -2,10 +2,11 @@
 
 pip freeze >  requirements.txt
 
-echo "Uvmowa@2024" | sudo -S systemctl stop redis
+echo "Uvmowa@2024" | sudo systemctl stop redis
 
 celery -A src.celery_worker control shutdown
 
 kill -9 $(lsof -i :8000)
 
+deactivate
 # curl -X 'POST' 'http://127.0.0.1:8000/upload/'   -H 'accept: application/json'   -H 'Content-Type: multipart/form-data'   -F 'file=@data/data.csv'
